d0fa5da7907aa69f27f6db0a5b7e34cbc44b7202
Aditya <adityasinghrawat96@gmail.com>
2019-05-12 06:40:51 +0530

    openshift port added

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser');
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 const URI = process.env.MONGODB_URI || 'mongodb://localhost/database';
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const IP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 const DB_NAME = process.env.DB_NAME;
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -73,4 +74,4 @@ app.get('/:param*', (req,res)=>{
   });
 });
 
-app.listen(PORT, ()=> console.log('Server listening on port' + PORT));
+app.listen(PORT, IP, ()=> console.log('Server listening on port' + PORT));
